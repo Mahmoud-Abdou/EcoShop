c13ae7eef958bf788325301efea54dbd1a3f8a45
mahmoud-abdou <mahmoudabdou.eg@gmail.com>
2022-07-14 19:09:18 +0200

    products frond end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 const App = () => {
   return ( 
-    <div className="App">
-    </div>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>} exact/>
+            <Route path='/product/:id' element={<ProductScreen/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
